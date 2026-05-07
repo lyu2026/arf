b8476feb046b945c4b4af3e3c129f7a8dabab2e2
lyu2026 <lyuw2026@gmail.com>
2026-05-07 11:11:44 +0800

    .

--- www/js/diary.js
+++ www/js/diary.js
@@ -7,6 +7,10 @@ window.IX={
 	observer:{},
 	K:cordova.plugin.koofr,
 	S:cordova.plugin.sqlite,
+	N:cordova.plugin.notify,
+
+	FM:{},MS:['普通','开心','伤心','崩溃','愤怒','压抑','恐惧','惊讶','感动','期待','紧张','抓狂','满足','疲惫','慵懒','绝望'],
+	loader:`<svg viewBox='0 0 100 100' width='40' height='40'><style>path{fill:#fff}path:nth-child(1){animation:r1 2s linear infinite;transform-origin:50px 50px}path:nth-child(2){animation:r2 1s linear infinite;transform-origin:50px 50px}path:nth-child(3){animation:r1 2s linear infinite;transform-origin:50px 50px}@keyframes r1{to{transform:rotate(360deg)}}@keyframes r2{to{transform:rotate(-360deg)}}</style><path d='M31.6,3.5C5.9,13.6-6.6,42.7,3.5,68.4c10.1,25.7,39.2,38.3,64.9,28.1l-3.1-7.9c-21.3,8.4-45.4-2-53.8-23.3c-8.4-21.3,2-45.4,23.3-53.8L31.6,3.5z'/><path d='M42.3,39.6c5.7-4.3,13.9-3.1,18.1,2.7c4.3,5.7,3.1,13.9-2.7,18.1l4.1,5.5c8.8-6.5,10.6-19,4.1-27.7c-6.5-8.8-19-10.6-27.7-4.1L42.3,39.6z'/><path d='M82,35.7C74.1,18,53.4,10.1,35.7,18S10.1,46.6,18,64.3l7.6-3.4c-6-13.5,0-29.3,13.5-35.3s29.3,0,35.3,13.5L82,35.7z'/></svg>`,
 
 	ftime:ts=>{
 		const wd=['日','一','二','三','四','五','六'],mc=['正月','二月','三月','四月','五月','六月','七月','八月','九月','十月','冬月','腊月']
@@ -69,17 +73,20 @@ window.IX={
 				m=x.m
 				if(i===0)d=x.d
 				gbox.append($O.node('grid-c',{my:''},`${m} ${y}`))
-				gbox.append($O.node('grid-c',{dr:''},`<div I='${s[i].id}'><div L><div>${x.w}</div>${x.d}</div><div R><button onclick='run("IX","remove",WI)(this)'>删除</button><div F><div>${s[i].title}</div><div>${s[i].content}</div><div>${x.t}</div></div></div></div>`))
+				gbox.append($O.node('grid-c',{dr:''},`<div I='${s[i].id}' onclick='run("IX","add",WI)(this)'><div L><div>${x.w}</div>${x.d}</div><div R><button onclick='event.stopPropagation();run("IX","remove",WI)(this)'>删除</button><div F><div>${s[i].title}</div><div>${s[i].content}</div><div>${x.t}</div></div></div></div>`))
 				continue
 			}
-			gbox.$(':scope>grid-c[dr]:last-child').append($O.node('div',{I:s[i].id},`<div L><div>${x.w}</div>${x.d}</div><div R><button onclick='run("IX","remove",WI)(this)'>删除</button><div F><div>${s[i].title}</div><div>${s[i].content}</div><div>${x.t}</div></div></div>`))
+			gbox.$(':scope>grid-c[dr]:last-child').append($O.node('div',{I:s[i].id,onclick:'run("IX","add",WI)(this)'},`<div L><div>${x.w}</div>${x.d}</div><div R><button onclick='event.stopPropagation();run("IX","remove",WI)(this)'>删除</button><div F><div>${s[i].title}</div><div>${s[i].content}</div><div>${x.t}</div></div></div>`))
 		}
 		go&&go(true)
 	},
 
 	remove:async(me)=>{ // 删除记录
-		if(!confirm('你确定删除此记录吗？'))return
-		const $=me.closest('div[I]').sa('wait'),$p=$.closest('grid-c[dr]')
+		if(IX.wait||!confirm('你确定删除此记录吗？'))return
+		IX.wait=true
+		const $=me.sa('wait').closest('div[I]'),$p=$.closest('grid-c[dr]')
+		$.$('[F]').sa('wait')
+		$.$('button').html(IX.loader)
 		const id=$.ga('I'),s=await IX.S.select('O',{cs:['imgs','files'],w:id}).then(_=>{
 			const o=[]
 			o.push(...(_.files||[]).filter(_=>_.startsWith('/tyan/files/')))
@@ -87,22 +94,27 @@ window.IX={
 			return o
 		})
 		s.push(id+'.json')
-		log('线上文件，待删清单',s)
-		let o=await IX.K.remove('tyan',s).catch(_=>{
-			log('线上删除，操作失败',_)
-			return null
-		})
-		if(!o)return
+		let o=await IX.K.remove('tyan',s).catch(_=>log('线上删除，操作失败',_))
+		if(!o){
+			IX.wait=false
+			me.da('wait')
+			return
+		}
 		o=await IX.S.remove('O',id)
 		if(o<1){
 			log('本地数据，删除失败，请刷新重试')
+			IX.wait=false
+			me.da('wait')
 			return
 		}
-		$.remove()
-		if($p.children.length>0)return
-		const x=$p.previousElementSibling
-		if(x.ha('my'))x.remove()
-		$p.remove()
+		setTimeout(()=>{
+			$.remove()
+			if($p.children.length>0)return
+			const x=$p.previousElementSibling
+			if(x.ha('my'))x.remove()
+			IX.wait=false
+			$p.remove()
+		},5000)
 	},
 
 	calendar:async(me)=>{ // 日历
@@ -113,37 +125,93 @@ window.IX={
 		// cordova.plugin.orient.set('H')
 		log('已经转屏')
 
-		await cordova.plugin.notify.send({id:13,title:'看考场',text:'刚刚好的方法媳妇儿',num:4})
+		await cordova.plugin.notify.send({id:13,title:'看考场',text:'刚刚好的方法媳妇儿',num:4,page:'hj'})
 		log('设置成功')
 
 	},
 
-	add:async()=>{ // 新增
+	add:async(me)=>{ // 新增
+		if(IX.wait)return
+		const K=IX.key=crypto.randomUUID()
+		const mbox=$O.$('modal-c').html(''),id=IX.id=parseInt(me?.ga('I')||0)
+		if(id>0)mbox.html(`<sk pt30 f fv g12><sk f g12 h80></sk><sk f g12 h180></sk><sk f h60><sk q w60 h60></sk><sk q w60 h60></sk><sk q w60 h60></sk></sk><sk q h40></sk><sk q h30></sk></sk>`)
+		$O.body.sa('ns')
+		$O.$('grid').da('a')
+		$O.$('modal').da('hide').$('modal-t>title').html((id>0?'编辑':'添加')+'日记')
+		const {title,content,address,location,mood,tags,imgs,files}=id>0?await IX.S.select('O',id):{},[lng,lat]=location?.split(',')||['','']
+		if(id>0&&!title)return IX.modal_close()
+		mbox.html(`
+		<div x='title'><textarea placeholder=' '>${title||''}</textarea><label>日志标题</label></div>
+		<div x='content'><textarea placeholder=' '>${content||''}</textarea><label>日志内容</label></div>
+		<div x='mood' ph='当前心情'>${IX.MS.map(_=>`<span${!mood&&_=='普通'||mood==_?' c':''} onclick='run("IX","mood",WI)(this)'>${_}</span>`).join('')}</div>
+		<div x='tags'><input placeholder=' ' value='${(tags||[]).join(' ')}'/><label>日志标签，空格分割</label></div>
+		<div x='address'><input placeholder=' ' value='${address||''}'/><label>当前地址，手动输入/自动定位</label><span onclick='run("IX","location",WI)(this)'>🎯</span></div>
+		<div xx><div x='lng'><input readonly placeholder=' ' value='${lng||''}'/><label>当前经度</label></div><div x='lat'><input readonly placeholder=' ' value='${lat||''}'/><label>当前纬度</label></div></div>
+		<div x='imgs' ph='日志图片'>${imgs&&imgs.length>0?imgs.map(_=>`<img src='${_}'/>`).join(''):''}<div onclick='run("IX","upload",WI)(this)'>╋</div></div>
+		<div x='files' ph='日志附件'>${files&&files.length>0?files.map(_=>`<img src='${IX.FM[_.split('.').pop()]}'/>`).join(''):''}<div onclick='run("IX","ufile",WI)(this,"file")'>╋</div></div>
+		<button onclick='run("IX","save",WI)(this)'>保存</button>`)
+	},
 
-		await cordova.plugin.notify.badge(45)
-		log('设置成功')
+	mood:me=>{
+		$O.$$(`[x='mood']>*`).forEach(_=>_.da('c'))
+		me.sa('c')
+	},
 
-		const title='挖掘客户举报检测方法关系还记得盖好'
-		const content='顾虑感觉刚放假你好哥哥很多地方个非常喜欢好看'
-		const address='中国.黑龙江.漠河',location='45.89666,86.88556'
-		const mood='said',tags='徐',imgs=['https://pixabay.com/zh/images/download/x-10222434_1920.jpg']
-		let o=await IX.S.insert('O',{title,content,address,location,mood,tags,imgs,files:[]},true,true)
-		if(!o||!o.id||o.id<1){
-			log('添加失败','error')
+	location:me=>{
+		let o=await IX.GO.perm(true).catch(_=>'deny')
+		if(o=='deny')return
+		const {lng,lat}o=await IX.GO.get(true)
+		if(!lng||!lat)return
+		o=await IX.GX.rev(lat,lng,{max:3,lc:'zh'})
+		log(lng,lat,o)
+	},
+
+	upload:(me,tp='img')=>{
+		
+	},
+
+	save:async(me)=>{
+		if(IX.wait)return
+		IX.wait=true
+		$O.$('modal-c>button').html(IX.loader).sa('wait')
+		const id=IX.id,title=$O.$(`modal-c [x='title']>textarea`).value.trim()
+		if(!title){
+			log('日志编辑，内容不能为空')
+			IX.wait=false
 			return
 		}
-		log('已添数据',o)
-
-		const id=o.id,uo=await IX.K.upload('tyan',`${o.id}.json`,Array.from(new Uint8Array(new TextEncoder().encode(JSON.stringify(o)))))
+		const content=$O.$(`modal-c [x='content']>textarea`).value.trim()
+		if(!content){
+			log('日志编辑，内容不能为空')
+			IX.wait=false
+			return
+		}
+		const mood=$O.$(`modal-c [x='mood']>[c]`).innerText
+		const tags=$O.$(`modal-c [x='tags']>input`).value.trim().split(' ').map(_=>_.trim()).filter(Boolean)
+		const address=$O.$(`modal-c [x='address']>input`).value.trim()
+		const location=`${$O.$(`modal-c [x='lng']>input`).value.trim()||'0'},${$O.$(`modal-c [x='lat']>input`).value.trim()||'0'}`
+		let imgs=[],files=[],o
+		if(id>0)o=await IX.S.modify('O',{id,title,content,address,location,mood,tags,imgs,files},id,true,true)
+		else o=await IX.S.insert('O',{title,content,address,location,mood,tags,imgs,files},true,true)
+		if(!o||!o.id||o.id<1){
+			log('操作失败','error')
+			IX.wait=false
+			return
+		}
+		log(`已${id>0?'改':'添'}记录`,o)
+		const uo=await IX.K.upload('tyan',`${id}.json`,Array.from(new Uint8Array(new TextEncoder().encode(JSON.stringify(o)))))
 		log('已传数据',uo.o.name+' '+uo.o.hash)
+		IX.modal_close()
+		const tk='diary_tab'.gc('statistics')
+		if(tk)$O.$(`tab>[v='${tk}']`)?.click()
+	},
 
-		if('diary_tab'.gc()!='list')return
-
-		o=await IX.S.select('O',{cs:'at',w:id})
-		const {y,m,d,w,t}=IX.ftime(o.at)
-		if(!$O.$('grid-c'))$O.$('grid').append($O.node('grid-c',{my:''},`${m} ${y}`))
-		if(!$O.$('grid-c[dr]'))$O.$('grid').append($O.node('grid-c',{dr:''},`<div I='${id}'><div L><div>${w}</div>${d}</div><div R><button onclick='run("IX","remove",WI)(this)'>删除</button><div F><div>${title}</div><div>${content}</div><div>${t}</div></div></div></div>`))
-		else $O.$('grid>grid-c[dr]:last-child').append($O.node('div',{I:id},`<div L><div>${w}</div>${d}</div><div R><button onclick='run("IX","remove",WI)(this)'>删除</button><div F><div>${title}</div><div>${content}</div><div>${t}</div></div></div>`))
+	modal_close:()=>{ // 关闭详情弹层
+		IX.id=null
+		IX.wait=false
+		$O.$('modal').sa('hide','_I').$('modal-t>title').html('')
+		$O.$('modal-c').html('')
+		$O.body.da('ns')
 	},
 
 	watch:()=>{
@@ -152,8 +220,9 @@ window.IX={
 			if(f.ha('bind'))return
 			f.sa('bind')
 			let sx=0,sy=0
-			f.addEventListener('touchstart',e=>{sx=e.touches[0].clientX;sy=e.touches[0].clientY},false)
+			f.addEventListener('touchstart',e=>{if(f.ha('wait'))return;sx=e.touches[0].clientX;sy=e.touches[0].clientY},false)
 			f.addEventListener('touchmove',e=>{
+				if(f.ha('wait'))return
 				let dx=e.touches[0].clientX-sx,dy=e.touches[0].clientY-sy
 				if(Math.abs(dx)>Math.abs(dy)&&dx<-10){
 					e.preventDefault()
@@ -163,7 +232,9 @@ window.IX={
 			},false)
 		}
 		$O.addEventListener('click',e=>{
-			if(!e.target.closest('grid-c[dr]>[I]>[R]>[F]')&&opened&&!opened.ha('wait'))opened.da('S')
+			if(!opened||opened.ha('wait'))return
+			if(e.target.closest('grid-c[dr]>[I]>[R]>[F]'))return
+			opened.da('S')
 		},false)
 		IX.observer.load_more=new IntersectionObserver((s,o)=>{
 			let last=null
@@ -186,7 +257,7 @@ window.IX={
 		log('进入页面，自定义样式')
 		$O.$('head>style[ix]').innerHTML=`
 body{display:flex!important;flex-direction:column!important}
-
+body>tab> *:last-child{margin-left:auto}
 grid-c{float:left;display:block;width:calc(100vw - 20px);height:auto}
 
 grid-c[summary]{margin-top:12px;display:grid;grid-template-columns:1fr 1fr;grid-template-rows:1fr 1fr 1fr 1fr;height:600px;color:rgba(0,0,0,.6);font-size:40px;font-weight:800;gap:10px}
@@ -205,31 +276,64 @@ grid-c[summary]>[peak]{grid-column:2;grid-row:4}
 grid-c[my]{background:rgba(0,0,0,0);color:black;font-size:26px;line-height:40px;color:black;padding:0}
 body[dark] grid-c[my]{color:white}
 
-grid-c[dr]{display:flex;flex-direction:column;border-radius:14px;background:rgba(0,0,0,.04);overflow:hidden;margin-bottom:5px}
+grid-c[dr]{border-radius:12px;display:flex;flex-direction:column;background:rgba(0,0,0,.04);overflow:hidden;margin-bottom:5px}
 body[dark] grid-c[dr]{background:rgba(255,255,255,.04)}
 grid-c[dr]:last-child{margin-bottom:36px}
-grid-c[dr]>[I]{display:flex;margin-bottom:1px;overflow:hidden;padding-left:6px}
+grid-c[dr]>[I]{display:flex;margin-bottom:1px;overflow:hidden}
 grid-c[dr]>[I]:last-child{margin-bottom:0}
-grid-c[dr]>[I]>[L]{flex-shrink:0;width:19%;aspect-ratio:3/3.5;padding:0 8px;background:#eee;transition:background .2s}
+grid-c[dr]>[I]>[L]{flex-shrink:0;width:14.5%;aspect-ratio:7/8;padding:0 4px;background:#f9f9f9;transition:background .2s}
 body[dark] grid-c[dr]>[I]>[L]{background:#2a2a2a}
-grid-c[dr]>[I]:first-child>[L]{z-index:4;background:translate;text-align:center;font-size:22px}
-grid-c[dr]>[I]:first-child>[L]::after{content:'';display:block;position:absolute;top:6%;left:15%;z-index:10;width:70%;height:88%;background:rgba(0,0,0,.2);border-radius:24px}
+grid-c[dr]>[I]:first-child>[L]{z-index:4;text-align:center;font-size:22px}
+grid-c[dr]>[I]:last-child>[L]{z-index:4;text-align:center;font-size:22px}
+grid-c[dr]>[I]:first-child>[L]::after{content:'';display:block;position:absolute;top:7%;left:15%;z-index:10;width:70%;height:86%;background:rgba(0,0,0,.2);border-radius:24px}
 body[dark] grid-c[dr]>[I]:first-child>[L]::after{background:rgba(255,255,255,.2)}
 grid-c[dr]>[I]:first-child>[L]>div{font-size:12px;margin:20px auto 4px auto}
 grid-c[dr]>[I]:not(:first-child)>[L]{z-index:0;color:rgba(0,0,0,0)}
 grid-c[dr]>[I]:not(:first-child)>[L] *{color:rgba(0,0,0,0)}
 
 grid-c[dr]>[I]>[R]{flex:1;min-width:0}
-grid-c[dr]>[I]>[R]>button{position:absolute;right:0;top:0;width:18.5%;height:100%;background:#e74c3c;color:#fff;border:none;font-size:18px;font-weight:500;display:flex;align-items:center;justify-content:center;z-index:1}
+grid-c[dr]>[I]>[R]>button{position:absolute;right:0;top:0;width:15%;height:100%;background:#e74c3c;color:#fff;border:none;font-size:18px;font-weight:500;display:flex;align-items:center;justify-content:center;z-index:1}
+grid-c[dr]>[I]>[R]>button[wait]{background:#e74c3cae}
 grid-c[dr]>[I]>[R]>[F]{width:100%;height:100%;overflow:hidden;z-index:2;background:#f9f9f9;display:flex;flex-direction:column;padding:6px 4px 6px 0;transition:transform .25s}
 body[dark] grid-c[dr]>[I]>[R]>[F]{background:#2a2a2a}
-grid-c[dr]>[I]>[R]>[F][S]{transform:translateX(-19%)}
+grid-c[dr]>[I]>[R]>[F][S]{transform:translateX(-15%)}
 grid-c[dr]>[I]>[R]>[F]>*:first-child{font-size:16px;color:#222;padding-bottom:6px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}
-grid-c[dr]>[I]>[R]>[F]>*:nth-child(2){flex:1;font-size:13px;line-height:1.84;height:30px;color:rgba(0,0,0,.8);padding-bottom:3px;overflow:hidden;display:-webkit-box;-webkit-line-clamp:2;-webkit-box-orient:vertical;word-break:break-all}
+grid-c[dr]>[I]>[R]>[F]>*:nth-child(2){flex:1;font-size:13px;line-height:1.4;color:rgba(0,0,0,.8);margin-bottom:3px;overflow:hidden;display:-webkit-box;-webkit-line-clamp:2;-webkit-box-orient:vertical;word-break:break-all}
 grid-c[dr]>[I]>[R]>[F]>*:last-child{font-size:10px;color:rgba(0,0,0,.6)}
 body[dark] grid-c[dr]>[I]>[R]>[F]>*:first-child{color:#fff}
 body[dark] grid-c[dr]>[I]>[R]>[F]>*:nth-child(2){color:rgba(255,255,255,.8)}
 body[dark] grid-c[dr]>[I]>[R]>[F]>*:last-child{color:rgba(255,255,255,.5)}
+
+modal-c>*,modal-c *{margin:0;padding:0}
+modal-c textarea,modal-c input{width:100%;resize:none;border-radius:2px;box-shadow:inset 0 0 5px rgba(0,0,0,.2);padding:16px 8px 8px;outline:0;margin:0;font-size:14px;color:black;background:rgba(0,0,0,0)}
+body[dark] modal-c textarea,body[dark] modal-c input{box-shadow:inset 0 0 5px rgba(255,255,255,.2);color:white}
+modal-c [x]>label{font-size:18px;height:auto;position:absolute;left:8px;top:50%;transform:translateY(-50%);transition:.2s}
+modal-c [x]>textarea:focus+label,modal-c [x]>textarea:not(:placeholder-shown)+label,modal-c [x]>input:focus+label,modal-c [x]>input:not(:placeholder-shown)+label{top:10px;font-size:10px;color:#007bff}
+modal-c [x][ph]::before{content:attr(ph);display:block;font-size:10px;position:absolute;height:auto;left:0;top:-2px;z-index:10;color:#007bff}
+modal-c [x='mood'][ph]::before{font-size:14px;top:-12px}
+modal-c [x='title']>textarea{height:60px;line-height:1.3;font-size:20px;margin-bottom:4px}
+modal-c [x='content']>textarea{font-size:18px;min-height:34vh;line-height:1.6;margin-bottom:4px}
+modal-c [x='mood']{margin:8px 0 4px 0;display:grid;grid-template-columns:repeat(5,1fr);gap:4px}
+modal-c [x='mood']>*,modal-c [x='address']>span{border-radius:4px;box-shadow:inset 0 0 5px rgba(0,0,0,.2);text-align:center;line-height:34px;color:black;font-size:14px}
+modal-c [x='mood']>*:hover,modal-c [x='address']>span:hover{box-shadow:inset 0 0 5px rgba(0,0,0,.4)}
+modal-c [x='mood']>*[c]{background:rgba(0,0,0,.4);color:white}
+body[dark] modal-c [x='mood']>*,body[dark] modal-c [x='address']>span{box-shadow:inset 0 0 5px rgba(255,255,255,.2);color:white}
+body[dark] modal-c [x='mood']>*:hover,body[dark] modal-c [x='address']>span:hover{box-shadow:inset 0 0 5px rgba(255,255,255,.4)}
+body[dark] modal-c [x='mood']>*[c]{background:rgba(255,255,255,.4);color:black}
+modal-c [x='tags']>textarea{line-height:40px;margin-bottom:4px}
+modal-c [x='address'],modal-c [xx]{margin:4px 0 4px 0;display:flex;gap:4px;height:40px}
+modal-c [x='address']>input,modal-c [xx]>*{flex:1}
+modal-c [x='address']>span{width:40px;border-radius:3px;font-size:24px;line-height:40px;text-align:center}
+modal-c [x='imgs'],modal-c [x='files']{padding-top:8px;margin-top:2px;display:flex;align-items:center;gap:4px;overflow-x:auto;border-bottom:1px solid rgba(0,0,0,.1)}
+body[dark] modal-c [x='imgs'],body[dark] modal-c [x='files']{border-color:rgba(255,255,255,.1)}
+modal-c [x='imgs']>*,modal-c [x='files']>*{display:block;width:50px;height:50px;font-size:30px;text-align:center;object-fit:cover;border:1px solid rgba(0,0,0,.2);border-radius:2px}
+modal-c [x='imgs']>*:hover,modal-c [x='files']>*:hover{border-color:rgba(0,0,0,.5)}
+body[dark] modal-c [x='imgs']>*,body[dark] modal-c [x='files']>*{border-color:rgba(255,255,255,.2)}
+body[dark] modal-c [x='imgs']>*:hover,body[dark] modal-c [x='files']>*:hover{border-color:rgba(255,255,255,.5)}
+modal-c>button{margin:8px 0 20px 0;line-height:60px;border:0;border-radius:6px;background:#831BF2EC;color:white;text-align:center;font-size:18px}
+modal-c>button[wait]{background:#831BF2AE}
+modal-c>button:not([wait]):hover{background:#7009E0FF}
+modal-c>button>svg{margin:6px auto;display:block;object-fit:contain}
 `
 		log('渲染页面，构建 DOM 树')
 		$O.$$('body>*:not(#w_logs)').forEach(_=>_.remove())
@@ -239,7 +343,6 @@ body[dark] grid-c[dr]>[I]>[R]>[F]>*:last-child{color:rgba(255,255,255,.5)}
 			<div v='list' onclick='run("IX","list",WI)(this)'>列表</div>
 			<div v='calendar' onclick='run("IX","calendar",WI)(this)'>日历</div>
 			<div onclick='run("IX","add",WI)(this)'>╋ 新条目</div>
-			<div onclick='run("IX","sync",WI)(this)'>同步</div>
 		</tab><grid></grid><modal hide><mbox><modal-t><title></title>
 		<icc onclick='run("IX","modal_close",WI)()'>╳</icc>
 		</modal-t><modal-c><textarea IT></textarea><textarea IC></textarea></modal-c></mbox></modal>`+($O.$('#w_logs')?.html(true)||''))

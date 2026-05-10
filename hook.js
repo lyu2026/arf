98e7fdc0bd206639dd86cc4432547e987eb1be25
lyu2026 <lyuw2026@gmail.com>
2026-05-10 17:00:01 +0800

    .

--- hook.js
+++ hook.js
@@ -12,10 +12,19 @@ module.exports=function(ctx){
 <trust-anchors><certificates src="system"/><certificates src="user"/></trust-anchors>
 </base-config></network-security-config>`)
 
-	// 注入manifest
-	const s=fs.readFileSync(m,'utf8')
-	if(!s.includes('networkSecurityConfig'))fs.writeFileSync(m,s.replace('<application','<application android:networkSecurityConfig="@xml/cors"'))
-	if(!s.includes('android.software.leanback'))fs.writeFileSync(m,s.replace('<application','<uses-feature android:name="android.software.leanback" android:required="false"/>\n<uses-feature android:name="android.hardware.touchscreen" android:required="false"/>\n<application'))
+	// 读取manifest
+	let s=fs.readFileSync(m,'utf8')
+
+	// 注入 networkSecurityConfig
+	if(!s.includes('networkSecurityConfig'))s=s.replace('<application','<application android:networkSecurityConfig="@xml/cors"')
+
+	// 注入 uses-feature (TV适配)
+	if(!s.includes('android.software.leanback'))s=s.replace('<application','<uses-feature android:name="android.software.leanback" android:required="false"/>\n<uses-feature android:name="android.hardware.touchscreen" android:required="false"/>\n<application')
+
+	// 注入 LEANBACK_LAUNCHER
+	if(!s.includes('LEANBACK_LAUNCHER'))s=s.replace('android.intent.category.LAUNCHER','android.intent.category.LAUNCHER\'/>\n                <category android:name=\'android.intent.category.LEANBACK_LAUNCHER')
+
+	fs.writeFileSync(m,s)
 
 	// Kotlin版本 + ABI过滤
 	fs.writeFileSync(p.join(b,'build-extras.gradle'),`configurations.all {

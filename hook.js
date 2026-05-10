724cf581c7e89fdc85605b697760fa1aa19abb22
lyu2026 <lyuw2026@gmail.com>
2026-05-10 17:07:39 +0800

    .

--- hook.js
+++ hook.js
@@ -1,4 +1,4 @@
-const fs=require('fs'),p=require('path')
+// const fs=require('fs'),p=require('path')
 
 module.exports=function(ctx){
 	const b=p.join(ctx.opts.projectRoot,'platforms','android','app')
@@ -21,8 +21,8 @@ module.exports=function(ctx){
 	// 注入 uses-feature (TV适配)
 	if(!s.includes('android.software.leanback'))s=s.replace('<application','<uses-feature android:name="android.software.leanback" android:required="false"/>\n<uses-feature android:name="android.hardware.touchscreen" android:required="false"/>\n<application')
 
-	// 注入 LEANBACK_LAUNCHER
-	if(!s.includes('LEANBACK_LAUNCHER'))s=s.replace('android.intent.category.LAUNCHER','android.intent.category.LAUNCHER\'/>\n                <category android:name=\'android.intent.category.LEANBACK_LAUNCHER')
+	// 注入 LEANBACK_LAUNCHER（追加，不替换）
+	if(!s.includes('LEANBACK_LAUNCHER'))s=s.replace(/<category android:name="android.intent.category.LAUNCHER" \/>/,'<category android:name="android.intent.category.LAUNCHER" />\n                <category android:name="android.intent.category.LEANBACK_LAUNCHER" />')
 
 	fs.writeFileSync(m,s)
 
